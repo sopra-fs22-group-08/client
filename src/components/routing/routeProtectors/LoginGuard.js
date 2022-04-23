02b6369565d3f67f7abc8403ebeb73f725a71505
arminveres <armin.veres@hotmail.com>
2022-04-23 19:47:14 +0200

    cleaned up some files
    Removed logout function, when clicking on user.
    Added .editorconfig file, which has some formatting
    standards.
    See https://editorconfig.org/

--- src/components/routing/routeProtectors/LoginGuard.js
+++ src/components/routing/routeProtectors/LoginGuard.js
@@ -1,19 +1,18 @@
-import {Redirect} from "react-router-dom";
-import PropTypes from "prop-types";
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 /**
  *
  * Another way to export directly your functional component.
  */
-export const LoginGuard = props => {
-  if (!localStorage.getItem("token")) {
-    return props.children;
-  }
-  // if user is already logged in, redirects to the main /app
-  const id = localStorage.getItem("userId")
-  return <Redirect to='/home/${id}' />;
+export const LoginGuard = (props) => {
+    if (!sessionStorage.getItem('token')) {
+        return props.children;
+    }
+    // if user is already logged in, redirects to the main /app
+    return <Redirect to='/home/${id}' />;
 };
 
 LoginGuard.propTypes = {
-  children: PropTypes.node
-}
+    children: PropTypes.node,
+};
