363da0b75db7bbe708bca745c319ad80bc22cfa2
aredin69 <andrin.reding@uzh.ch>
2022-04-23 12:27:40 +0200

    Card Creator done close #47 close #48 close #49

--- src/components/views/CardCreator.js
+++ src/components/views/CardCreator.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {api, handleError} from 'helpers/api';
 import User from 'models/User';
-import {useHistory} from 'react-router-dom';
+import {useHistory, useLocation} from 'react-router-dom';
 import {Button} from 'components/ui/Button';
-import 'styles/views/Register.scss';
+import 'styles/views/CardCreator.scss';
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 
@@ -17,9 +17,9 @@ specific components that belong to the main one in the same file.
 
 const FormFieldLn = props => {
   return (
-      <div className="card field">
+      <div className="cardCreator card-question-field">
         <input
-            className="card question-text"
+            className="cardCreator card-text"
             placeholder="Enter the Question ..."
             value={props.value}
             onChange={e => props.onChange(e.target.value)}
@@ -35,9 +35,9 @@ FormFieldLn.propTypes = {
 
 const FormFieldEm = props => {
   return (
-      <div className="card field">
+      <div className="cardCreator card-aw-field">
         <input
-            className="card answer-text"
+            className="cardCreator card-text"
             placeholder="Enter the Answer ..."
             value={props.value}
             onChange={e => props.onChange(e.target.value)}
@@ -53,9 +53,9 @@ FormFieldEm.propTypes = {
 
 const FormFieldFn = props => {
     return (
-            <div className="card field">
+            <div className="cardCreator card-waw1-field">
                 <input
-                        className="card wrongAnswer-text"
+                        className="cardCreator card-text"
                         placeholder="Enter a wrong Answer ..."
                         value={props.value}
                         onChange={e => props.onChange(e.target.value)}
@@ -70,9 +70,9 @@ FormFieldFn.propTypes = {
 };
 const FormFieldUn = props => {
     return (
-            <div className="card field">
+            <div className="cardCreator card-waw2-field">
                 <input
-                        className="card wrongAnswer-text"
+                        className="cardCreator card-text"
                         placeholder="Enter a wrong Answer ..."
                         value={props.value}
                         onChange={e => props.onChange(e.target.value)}
@@ -87,9 +87,9 @@ FormFieldUn.propTypes = {
 };
 const FormFieldPw = props => {
     return (
-            <div className="card field">
+            <div className="cardCreator card-waw3-field">
                 <input
-                        className="card wrongAnswer-text"
+                        className="cardCreator card-text"
                         placeholder="Enter a wrong Answer ..."
                         value={props.value}
                         onChange={e => props.onChange(e.target.value)}
@@ -104,14 +104,46 @@ FormFieldPw.propTypes = {
 };
 
 
+
+
+
+
+
 const CardCreator = () => {
   const history = useHistory();
+  const location = useLocation();
   const [question, setQuestion] = useState(null);
   const [answer, setAnswer] = useState(null);
   const [wrongAnswer1, setWrongAnswer1] = useState(null);
   const [wrongAnswer2, setWrongAnswer2] = useState(null);
   const [wrongAnswer3, setWrongAnswer3] = useState(null);
   const options = [answer, wrongAnswer1, wrongAnswer2, wrongAnswer3];
+  const [burgerMenu, setBurgerMenu] = useState(false);
+  const [user, setUser] = useState(null);
+
+
+
+    useEffect(() => {
+        // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
+        async function fetchData() {
+            try {
+                const userId = localStorage.getItem("userId")
+                const response = await api.get('/users/' + userId);
+
+                await new Promise(resolve => setTimeout(resolve, 1000));
+
+                setUser(response.data)
+
+
+            } catch (error) {
+                console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
+                console.error("Details:", error);
+                alert("Something went wrong while fetching the users! See the console for details.");
+            }
+        }
+
+        fetchData();
+    }, []);
 
   const doCardCreator = async () => {
     try {
@@ -127,26 +159,103 @@ const CardCreator = () => {
     }
   };
 
+    const logout = () => {
+        localStorage.removeItem('token');
+        history.push('/login');
+    }
+
+    const goProfile = async () => {
+        const id = localStorage.getItem("userId")
+        history.push(`/profile/` + id);
+
+    };
+
+    const goHome = async () => {
+        const id = localStorage.getItem("userId")
+        history.push(`/home/` + id);
+
+    };
+
+    const goStore = async () => {
+        history.push(`/store`);
+
+    };
+
+    const goCreator = async () => {
+        history.push(`/creator`);
+
+    };
+
+    let burgerMenuContent = (
+        <BaseContainer>
+            <div className="cardCreator window"></div>
+            <div className="cardCreator username"></div>
+            <Button
+                className="cardCreator username"
+                onClick={() => {setBurgerMenu(false); goProfile();}}
+            >{user?.username  ? user.username : "Username"}
+            </Button>
+            <Button
+                className="cardCreator home"
+                onClick={() => goHome()}
+            >Home
+            </Button>
+            <Button
+                className="cardCreator store"
+                onClick={() => goStore()}
+            >Store
+            </Button>
+            <Button
+                className="cardCreator creator"
+                onClick={() => goCreator()}
+            >Creator
+            </Button>
+            <Button
+                className="cardCreator logoutButton"
+                onClick={() => logout()}
+            >Logout
+            </Button>
+            <div
+                className="cardCreator x"
+                onClick={() => setBurgerMenu(false)}
+
+            >x</div>
+
+        </BaseContainer>
+    )
+
 
   document.body.style = 'background: #4757FF;';
 
   return (
 
     <BaseContainer>
-      <div className="carddeck title">No Brainer</div>
-      <div className="carddeck login-text">Create New Card</div>
+        <div className="cardCreator title">NB</div>
 
 
-      <div className="card frontText-title">Front Text</div>
-      <div className="card frontText-field"></div>
+        <div className="cardCreator burger1"></div>
+        <div className="cardCreator burger2"></div>
+        <div className="cardCreator burger3"></div>
+        <div
+            className="cardCreator burgerButton"
+            // open edit window
+            onClick={() => setBurgerMenu(true)}
+        ></div>
+
+
+      <div className="cardCreator card-title">Card</div>
+
+
+      <div className="cardCreator card-ft">Question</div>
+      <div className="cardCreator frontText-field"></div>
 
       <FormFieldLn
           value={question}
           onChange={n => setQuestion(n)}
       />
 
-      <div className="card backText-title">Back Text</div>
-      <div className="card backText-field"></div>
+      <div className="cardCreator card-bt">Answer</div>
+      <div className="cardCreator backText-field"></div>
 
       <FormFieldEm
           value={answer}
@@ -154,23 +263,23 @@ const CardCreator = () => {
       />
 
 
-        <div className="card wrongAnswer-title">Wrong Answer 1</div>
-        <div className="card wrongAnswer-field"></div>
+        <div className="cardCreator card-w1">Wrong Answer</div>
+        <div className="cardCreator wrongAnswer-field"></div>
 
         <FormFieldFn
                 value={wrongAnswer1}
                 onChange={n => setWrongAnswer1(n)}
         />
 
-        <div className="card wrongAnswer-title">Wrong Answer 2</div>
-        <div className="card wrongAnswer-field"></div>
+        <div className="cardCreator card-w2">Wrong  Answer</div>
+        <div className="cardCreator wrongAnswer-field"></div>
 
         <FormFieldUn
                 value={wrongAnswer2}
                 onChange={n => setWrongAnswer2(n)}
         />
 
-        <div className="card wrongAnswer-title">Wrong Answer 3</div>
+        <div className="cardCreator card-w3">Wrong Answer</div>
         <div className="card wrongAnswer-field"></div>
 
         <FormFieldPw
@@ -179,24 +288,22 @@ const CardCreator = () => {
         />
 
 
-
-
-
       <Button
-              className="card addCardButton"
+              className="cardCreator createButton3"
               disabled={question || answer || wrongAnswer1 || wrongAnswer2 || wrongAnswer3}
               onClick={() => history.push(`/home/` + 1)}
       >
           End
       </Button>
       <Button
-          className="cardDeck createButton"
+          className="cardCreator createButton4"
           disabled={!question || !answer || !wrongAnswer1 || !wrongAnswer2 || !wrongAnswer3}
           onClick={() => doCardCreator()}
       >
         Create
       </Button>
 
+        {burgerMenu ? burgerMenuContent : null}
     </BaseContainer>
   );
 };
