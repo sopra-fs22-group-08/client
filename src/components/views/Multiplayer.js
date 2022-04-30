197ca44b09c0a764213e17d7cb9d26ed4d5fd0b9
tportmann-uzh <tim.portmann@uzh.ch>
2022-04-30 20:44:50 +0200

    Fixed Intervall Bug in MP Game

--- src/components/views/Multiplayer.js
+++ src/components/views/Multiplayer.js
@@ -1,29 +1,32 @@
-import BaseContainer from "../ui/BaseContainer";
-import {useHistory, useLocation} from "react-router-dom";
-import React, {useEffect, useState} from "react";
-import {api, handleError} from "../../helpers/api";
-import LoadingSpin from "react-loading-spin";
+import BaseContainer from '../ui/BaseContainer';
+import {useHistory, useLocation} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {api, handleError} from '../../helpers/api';
+import LoadingSpin from 'react-loading-spin';
 import 'styles/views/Multiplayer.scss';
 import 'styles/ui/LoadingScreenButton.scss';
 import {Button} from 'components/ui/Button';
 import declineInvite from 'components/views/Home';
 
 const Multiplayer = () => {
+
+    const [hasAccepted, setHasAccepted] = useState(false);
+    const [duel, setDuel] = useState(null);
+
     const history = useHistory();
     const location = useLocation();
 
-    async function checkAccepted() {
+    const checkAccepted = async () => {
         try {
-            const duelId = localStorage.getItem("duelId");
+            const duelId = localStorage.getItem('duelId');
             const responseDuel = await api.get('/duels/' + duelId);
 
             await new Promise((resolve) => setTimeout(resolve, 1000));
-
             const d = responseDuel.data;
 
-            if (d.playerTwoStatus === "ACCEPTED") {
-                localStorage.setItem("duelId", d.id);
-                history.push('/multiplayerTool/deckID=' + d.deckId + '/cardID=0');
+            if (d.playerTwoStatus === 'ACCEPTED') {
+                setHasAccepted(true);
+                setDuel(d);
             }
 
         } catch (error) {
@@ -33,45 +36,60 @@ const Multiplayer = () => {
     }
 
     useEffect(() => {
-
-        const interval = setInterval(() => {
-            checkAccepted();
-        }, 5000);
-
+        setInterval(() => checkAccepted(), 5000);
     }, []);
 
     document.body.style = 'background: #FFCA00;';
     return (
         <BaseContainer>
-            <div className="Loading">
-                <div className="Loading text">
-                    waiting for your friend to accept
+
+            {hasAccepted
+                ?
+                <div className="Loading text-Start">
+                    Your game is ready
+                    <div className="Loading start">
+                        <Button
+                            className='loadingScreen-button'
+                            onClick={() => {
+                                localStorage.setItem('duelId', duel.id);
+                                history.push('/multiplayerTool/deckID=' + duel.deckId + '/cardID=0');
+                            }}>
+                            Start Game
+                        </Button>
+                    </div>
                 </div>
-                <div className={"ExampleOfUsage"}>
-                    <LoadingSpin
-                        duration="2s"
-                        width="15px"
-                        timingFunction="ease-in-out"
-                        direction="alternate"
-                        size="150px"
-                        primaryColor="yellow"
-                        secondaryColor="#333"
-                        numberOfRotationsInAnimation={2}
-                    />
+                :
+                <div className="Loading">
+                    <div className="Loading text">
+                        waiting for your friend to accept
+                    </div>
+                    <div className={"ExampleOfUsage"}>
+                        <LoadingSpin
+                            duration="2s"
+                            width="15px"
+                            timingFunction="ease-in-out"
+                            direction="alternate"
+                            size="150px"
+                            primaryColor="yellow"
+                            secondaryColor="#333"
+                            numberOfRotationsInAnimation={2}
+                        />
+                    </div>
+                    <div className="Loading cancel">
+                        <Button
+                            className='loadingScreen-button'
+                            onClick={() => {
+                                api.delete('/invitations/' + location.state.detail.id);
+                                history.push(`/home/` + localStorage.getItem("userId"));
+                            }}>
+                            cancel
+                        </Button>
+                    </div>
                 </div>
-                <div className="Loading cancel">
-                    <Button
-                        className='loadingScreen-button'
-                        onClick={() => {
-                            api.delete('/invitations/' + location.state.detail.id);
-                            history.push(`/home/` + localStorage.getItem("userId"));
-                        }}>
-                        cancel
-                    </Button>
-                </div>
-            </div>
+            }
+
         </BaseContainer>
     );
-}
+};
 
 export default Multiplayer;
