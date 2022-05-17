469f0282fae091fc31b78209815779bb61f8b0e9
arminveres <armin.veres@hotmail.com>
2022-05-17 17:44:41 +0200

    added search feature and view (needs improvement) close #39

--- src/components/views/Library.js
+++ src/components/views/Library.js
@@ -1,48 +1,59 @@
 import 'styles/views/Home.scss';
+import 'styles/views/Profile.scss';
 // react imports
 import React, { useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 // local imports
 import { api, handleError } from 'helpers/api';
 import BaseContainer from 'components/ui/BaseContainer';
 import { Button } from 'components/ui/Button';
 import Header from 'components/ui/Header';
 
+const FormFieldDeckSearch = (props) => {
+    return (
+        <div className='profile field'>
+            <input
+                className='profile lastName-text'
+                placeholder='Enter Deck Name'
+                value={props.value}
+                onChange={(input) => {
+                    props.onChange(input.target.value);
+                }}
+            />
+        </div>
+    );
+};
+
+FormFieldDeckSearch.propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+};
+
 const Library = () => {
+    document.body.style = 'background: #4757FF';
+
     const history = useHistory();
-    const location = useLocation();
 
     const [user, setUser] = useState(null);
-    const [allDecks, setAllDecks] = useState(null);
-
-    // TODO: remove userData, doesn't do anything, when connecting it is still empty
-    const [userData, setUserData] = useState({
-        from: '',
-        connected: false,
-    });
+    const [publicDecks, setPublicDecks] = useState(null);
+    const [searchString, setSearchString] = useState('');
+    const [foundDecks, setFoundDecks] = useState(null);
 
     const cardOverview = () => {
-        localStorage.setItem('edit', false)
+        localStorage.setItem('edit', false);
         history.push('/cardOverview');
     };
 
-    function isPublic(de) {
-        if (String(de.visibility) === 'PUBLIC'){
-            return de;
-        }
-    }
-
     useEffect(() => {
-
         async function fetchData() {
             try {
                 const userId = localStorage.getItem('userId');
                 const responseUser = await api.get('/users/' + userId);
-                const responseDecks = await api.get('/decks');
-                //await new Promise((resolve) => setTimeout(resolve, 1000));
+                // NOTE: get only public decks, so they don't have to get filtered further down
+                const responseDecks = await api.get('/decks/visibility/PUBLIC');
                 setUser(responseUser.data);
-                setAllDecks(responseDecks.data);
-
+                setPublicDecks(responseDecks.data);
             } catch (error) {
                 console.error(
                     `Something went wrong while fetching the users: \n${handleError(error)}`
@@ -56,39 +67,117 @@ const Library = () => {
         fetchData();
     }, []);
 
+    /**
+     * @brief creates an assortment of inputted Decks
+     */
+    const createDeckView = (inputDecks) => {
+        return inputDecks.map((d) => (
+            <Button
+                className='Home listElement-Box'
+                onClick={() => {
+                    cardOverview();
+                    localStorage.setItem('deckId', d.id);
+                }}
+            >
+                <div className='Home listElement-Number' />
+                <div className='Home listElement-Title'>{d.deckname}</div>
+                <div className='Home listElement-Score'>
+                    <br /> <br />{' '}
+                </div>
+                <div className='Home listElement-Text'>Click to ADD</div>
+            </Button>
+        ));
+    };
 
     if (user) {
         var listItems = <div className='Home deck-None'>Please create a new Deck</div>;
-        if (allDecks) {
-            const publicDecks = allDecks.filter(isPublic)
-            console.log(publicDecks)
-            listItems = publicDecks.map((d) => (
-                <Button
-                    className='Home listElement-Box'
-                    onClick={() => {
-                        cardOverview();
-                        localStorage.setItem('deckId', d.id);
-                    }}
-                >
-                    <div className='Home listElement-Number'/>
-                    <div className='Home listElement-Title'>{d.deckname}</div>
-                    <div className='Home listElement-Score'>
-                        <br /> <br />{' '}
-                    </div>
-                    <div className='Home listElement-Text'>Click to ADD</div>
-                </Button>
-            ));
+        if (publicDecks) {
+            // console.log(publicDecks);
+            listItems = createDeckView(publicDecks);
         }
-
     }
 
-    document.body.style = 'background: #4757FF';
+    /**
+     * @brief fetches decks with a searchString
+     */
+    const getSearchedForDecks = async (input) => {
+        try {
+            if (input !== '') {
+                console.log(input);
+                const responseDecks = await api.get('/decks/search/' + input);
+                console.log(responseDecks.data);
+                setFoundDecks(responseDecks.data);
+                // now rewrite the listItems
+                listItems = createDeckView(foundDecks);
+            } else {
+                alert('Please enter a search String');
+            }
+        } catch (error) {
+            alert('No deck found, please retry with another string');
+            console.error(`Something went wrong while fetching the decks: \n${handleError(error)}`);
+        }
+    };
 
-    return (
+    // TODO: find better postioning and do better styling
+    /**
+     * @brief element including search elements
+     */
+    let SearchElement = (
+        <BaseContainer>
+            <FormFieldDeckSearch
+                value={searchString}
+                onChange={(input) => {
+                    setSearchString(input);
+                }}
+            />
+            <Button
+                className='profile createButton'
+                onClick={() => {
+                    getSearchedForDecks(searchString);
+                }}
+            >
+                Submit
+            </Button>
+        </BaseContainer>
+    );
+
+    let publicDecksView = (
         <BaseContainer>
             <div className='Home listTitle'>Public Decks</div>
             <div className='Home list'>{listItems}</div>
-            <Header/>
+        </BaseContainer>
+    );
+
+    let searchedDecksView = (
+        <BaseContainer>
+            <div className='Home listTitle'>Found Decks</div>
+            <div className='Home list'>{listItems}</div>
+        </BaseContainer>
+    );
+
+    // TODO: find better styling and better positioning
+    /**
+     * @brief this reloads the page, resulting in getting back to all
+     * public decks
+     */
+    let reloadPage = (
+        <BaseContainer>
+            <Button
+                onClick={() => {
+                    window.location.reload(true);
+                }}
+            >
+                Go Back to all Decks
+            </Button>
+        </BaseContainer>
+    );
+
+    return (
+        <BaseContainer>
+            <Header />
+            {SearchElement}
+            {foundDecks === null ? publicDecksView : searchedDecksView}
+            {reloadPage}
         </BaseContainer>
     );
 };
